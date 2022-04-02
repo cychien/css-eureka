1832a90372e20727ed6a340daf1b342f88aabae6
Justin <justin@akohub.com>
2022-04-02 16:50:37 +0800

    Publish article

---
+++ posts/bfc/ExternalFloatOG.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+function ExternalFloatOG() {
+  return (
+    <div
+      className="not-prose"
+      style={{
+        display: "flow-root",
+        margin: "80px 0",
+      }}
+    >
+      <div
+        style={{
+          padding: "24px",
+          backgroundColor: "#fafaf9",
+          boxShadow:
+            "0 1px 3px 0 rgba(0 0 0 / 0.1), 0 1px 2px -1px rgba(0 0 0 / 0.1)",
+          borderRadius: "8px",
+        }}
+      >
+        <div
+          className="float"
+          style={{
+            float: "left",
+            height: "60px",
+            width: "60px",
+            backgroundColor: "#d6d3d1",
+            borderRadius: "4px",
+            marginRight: "16px",
+            fontSize: "14px",
+            padding: "4px",
+          }}
+        >
+          Floated div
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ExternalFloatOG;
---
+++ posts/bfc/InternalFloatOG2.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+function InternalFloatOG2() {
+  return (
+    <div
+      className="not-prose"
+      style={{
+        display: "flow-root",
+        margin: "80px 0",
+        backgroundColor: "#e0f2fe",
+      }}
+    >
+      <div
+        className="parent"
+        style={{ backgroundColor: "#bbf7d0", border: "2px solid #22c55e" }}
+      >
+        <div
+          className="float"
+          style={{
+            height: "100px",
+            float: "left",
+            backgroundColor: "#fed7aa",
+            border: "2px solid #f97316",
+          }}
+        >
+          I am a floated box!
+        </div>
+        <p>I am content inside the container.</p>
+      </div>
+    </div>
+  );
+}
+
+export default InternalFloatOG2;
